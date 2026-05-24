9eed9a6a51876107943288e9c2d9ca11fbbefde3
Prabhat Jha <prabhatjha1511@gmail.com>
2026-05-25 02:26:11 +0530

    Intial setup

---
+++ src/main/java/com/showtime/ShowtimeApplication.java
@@ -0,0 +1,13 @@
+package com.showtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowtimeApplication.class, args);
+    }
+
+}
